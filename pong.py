8ca6637b5a654f88ced125e4371df07d2703c000
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-14 06:36:46 +0530

    main file created and coded main part of game(game window size)

---
+++ pong.py
@@ -0,0 +1,12 @@
+import pygame 
+
+pygame.init()
+
+screen_width = 1280
+screen_height= 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("My Pong Game!")
+
+clock = pygame.time.clock()
+
