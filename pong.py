f98a95221fb322182e27f1e2ec2470b648a3e33b
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-14 09:17:02 +0530

    made cpu handle the paddle and user can handle paddle

--- pong.py
+++ pong.py
@@ -1,5 +1,39 @@
 import pygame , sys
 
+def animate_ball():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    
+    if ball.bottom >= screen_height or ball.top <= 0:
+        ball_speed_y *= -1
+    
+    if ball.right >= screen_width or ball.left <= 0:
+        ball_speed_x *= -1  
+        
+def animate_player():
+    player.y += player_speed
+    
+    if player.top <= 0:
+        player.top = 0
+    
+    if player.bottom >= screen_height:
+        player.bottom = screen_height 
+        
+def animate_cpu():
+    global cpu_speed
+    cpu.y += cpu_speed
+    if ball.centery <= cpu.centery:
+        cpu_speed = -6
+    if ball.centery >= cpu.centery:
+        cpu_speed = 6     
+    
+    if cpu.top <=0:
+        cpu.top = 0
+    if cpu.bottom >= screen_height:
+        cpu.bottom = screen_height    
+                   
+
 pygame.init()
 
 screen_width = 950
@@ -21,6 +55,8 @@ player.midright = (screen_width, screen_height/2)
 
 ball_speed_x = 6
 ball_speed_y = 6
+player_speed = 0
+cpu_speed = 6
 
 while True:
     #check the event
@@ -28,9 +64,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_UP:
+                player_speed = -6
+            if event.key == pygame.K_DOWN:
+                player_speed = 6 
+        if event.type == pygame.KEYUP:        
+            if event.key == pygame.K_UP:
+                player_speed = 0
+            if event.key == pygame.K_DOWN:
+                player_speed = 0        
+            
     #change the position of ball
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
+    animate_ball()
+    animate_player()
+    animate_cpu()
     
     
     #draw the game object 
